a87d5f9ea97e1f1f85da9bb9fb204dd9c1aae12c
DELL <DELL@DESKTOP-MHQEA3C>
2018-06-12 11:55:25 +0530

    commit by dev2 on branch1

--- App/src/com/nt/app/Test.java
+++ App/src/com/nt/app/Test.java
@@ -5,13 +5,15 @@ public void add() {
 	int x=10+5;
 	System.out.println(x);
 	}
+public String wishMsg(String name) {
+	return "good afternoon";
+}
 	public static void main(String[] args) {
 		System.out.println("Hello");
-	
 		Test t=new Test();
 		t.add();
 		System.err.println(t);
-
+        System.out.println(t.wishMsg("satya")); 
 	}
 
 }
