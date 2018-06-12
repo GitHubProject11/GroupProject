dc822c9b54318d2e5e8fdbc3ba9e2caa62bdf3be
Rajesh <Rajesh@DESKTOP-GKB890B>
2018-06-12 10:51:52 +0530

    commitd by tl1

--- App/src/com/nt/app/Test.java
+++ App/src/com/nt/app/Test.java
@@ -3,10 +3,12 @@ package com.nt.app;
 public class Test {
 public void add() {
 	int x=10+5;
+	System.out.println(x);
 	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		
+		Test t=new Test();
+		t.add();
 
 	}
 
